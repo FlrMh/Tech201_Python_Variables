b9f89dd094ff81d63a22c1e8d2bb8fcbf3e10190
FlrMh <florina.mihai110@gmail.com>
2023-01-27 15:57:54 +0000

    update

---
+++ Variables.py
@@ -0,0 +1,64 @@
+# Variables
+
+# variable_name = variable_data
+
+a = 1 #int
+b = 2 #int
+c = 3.5 #float
+hi = "Hello World!" #string
+
+# print(hi)
+# print(a)
+
+# When we set a variable, the app stores it, and we can use it later in the code.
+# Comment out multiple lines at the same time `Ctrl+/`
+
+# print(1 + 2)
+# print(a + b)
+
+# Dynamic type language = don t have to specify data type
+# Strong type languages = have to specify data type (e.g. int a = 1)
+
+
+# Most important method used in Python: the Type Method(check what type of data our variable is)
+
+print(type(hi))
+print(type(a))
+print(type(c))
+
+# Overwriting variables
+# print(a) - first time it will print 1 from the previous value
+# a = 4
+# print(a) - second time it will print 4 from the newly set value
+
+
+# Getting user input
+
+# print("Hi, what is your name?")
+# name = input()
+# print("Hi")
+# print(name)
+
+
+# Make a program that takes the user`s name, age and DOB and print them back out.
+
+print("Hello, what is your name?")
+name = input()
+print("Hi")
+print(name)
+print("How old are you?")
+age = input()
+print("Just to confirm that this is your age?")
+print(age)
+input()
+print("Great!So what is your date of birth?")
+DOB = input()
+print("So this is your date of birth?")
+print(DOB)
+print("Thank you!")
+
+
+
+
+
+
